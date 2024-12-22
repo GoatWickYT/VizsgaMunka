cb45dcafa987664ee4b00026d46e97e460531f95
Wittner Attila <wiattila8@gmail.com>
2024-12-22 11:27:42 +0100

    Movement+Anim base

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public bool isMoving;
-    public Vector2 input;
+    public float MoveSpeed;
+    private Vector2 input;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("MoveX", input.x);
+        animator.SetFloat("MoveY", input.y);
+
+        var targetPos = transform.position;
+
+        targetPos.x += input.x;
+        targetPos.y += input.y;
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, MoveSpeed * Time.deltaTime);
+
+    }
 }
